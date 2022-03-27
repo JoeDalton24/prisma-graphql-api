82763e78df8f644a6417fc86b70ea19fb0496a13
Joedalto24 <kairahima32@gmail.com>
2022-03-27 16:03:51 +0000

    feat: :sparkles: lock down all query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,5 @@
+import { getUserId } from "../utils/util.js";
+
 const Query = {
   users(parent, args, { prisma }, info) {
     const optionArg = {};
@@ -15,25 +17,84 @@ const Query = {
     }
     return prisma.query.users(optionArg, info);
   },
-  posts(parent, args, { prisma }, info) {
+
+  posts(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request, false);
+
     const optionArg = {};
-    if (args.query) {
+
+    if (userId === null) {
+      optionArg.where = {
+        published: true,
+      };
+    }
+
+    if (userId) {
       optionArg.where = {
         OR: [
+          { published: true },
           {
-            title_contains: args.query,
-          },
-          {
-            body_contains: args.query,
+            author: {
+              id: userId,
+            },
           },
         ],
       };
     }
+
+    if (args.query) {
+      const previousOption = { ...optionArg.where };
+      optionArg.where = {
+        AND: [
+          { ...previousOption },
+          {
+            OR: [
+              {
+                title_contains: args.query,
+              },
+              {
+                body_contains: args.query,
+              },
+            ],
+          },
+        ],
+      };
+    }
+
     return prisma.query.posts(optionArg, info);
   },
 
   comments(parent, args, { prisma }, info) {
     return prisma.query.comments(null, info);
+  },
+
+  async post(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request, false);
+
+    const posts = await prisma.query.posts({
+      where: {
+        id: args.postId,
+        OR: [
+          { published: true },
+          {
+            author: {
+              id: userId,
+            },
+          },
+        ],
+      },
+    });
+
+    if (!posts.length) {
+      throw new Error("posts not found");
+    }
+
+    return posts[0];
+  },
+
+  me(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+    return prisma.query.user({ where: { id: userId } }, info);
   },
 };
 export default Query;
--- src/utils/util.js
+++ src/utils/util.js
@@ -1,16 +1,19 @@
 import jwt from "jsonwebtoken";
 const ACCES_TOKEN = "276486c9aee6135040d20a9461daab70";
 
-export function getUserId(request) {
+export function getUserId(request, requireAuth = true) {
   const header = request.request.headers.authorization;
 
-  if (!header) {
+  if (header) {
+    const token = header.split(" ")[1];
+    const decoded = jwt.verify(token, ACCES_TOKEN);
+    return decoded.userId;
+    //
+  }
+
+  if (requireAuth) {
     throw new Error("Authentification required");
   }
 
-  const token = header.split(" ")[1];
-
-  const decoded = jwt.verify(token, ACCES_TOKEN);
-
-  return decoded.userId;
+  return null;
 }
